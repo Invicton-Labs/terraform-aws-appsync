a98896f7f252ac3830c35e50132ea1beca9511fd
KyleKotowick <kotowick@invictonlabs.com>
2023-05-16 19:23:04 -0400

    Implement support for Lambda authorizers and custom domains

--- main.tf
+++ main.tf
@@ -45,10 +45,11 @@ EOF
 }
 
 // Create the GraphQL API
-resource "aws_appsync_graphql_api" "api" {
+resource "aws_appsync_graphql_api" "this" {
   authentication_type = var.authentication_types[0]
   name                = var.name
   schema              = var.schema
+  xray_enabled        = var.xray_enabled
   tags                = var.tags
 
   dynamic "log_config" {
@@ -64,10 +65,20 @@ resource "aws_appsync_graphql_api" "api" {
   dynamic "openid_connect_config" {
     for_each = var.authentication_types[0] == "OPENID_CONNECT" ? [1] : []
     content {
-      issuer    = lookup(var.openid_connect_config, "issuer", null)
-      auth_ttl  = lookup(var.openid_connect_config, "auth_ttl", null)
-      client_id = lookup(var.openid_connect_config, "client_id", null)
-      iat_ttl   = lookup(var.openid_connect_config, "iat_ttl", null)
+      issuer    = var.openid_connect_config.issuer
+      auth_ttl  = var.openid_connect_config.auth_ttl
+      client_id = var.openid_connect_config.client_id
+      iat_ttl   = var.openid_connect_config.iat_ttl
+    }
+  }
+
+  // Only put the lambda_authorizer_config block in the main block if OpenID is the first auth mechanism
+  dynamic "lambda_authorizer_config" {
+    for_each = var.authentication_types[0] == "AWS_LAMBDA" ? [1] : []
+    content {
+      authorizer_uri                   = var.lambda_authorizer_config.issauthorizer_uriuer
+      authorizer_result_ttl_in_seconds = var.lambda_authorizer_config.authorizer_result_ttl_in_seconds
+      identity_validation_expression   = var.lambda_authorizer_config.identity_validation_expression
     }
   }
 
@@ -75,10 +86,10 @@ resource "aws_appsync_graphql_api" "api" {
   dynamic "user_pool_config" {
     for_each = var.authentication_types[0] == "AMAZON_COGNITO_USER_POOLS" ? [1] : []
     content {
-      default_action      = lookup(var.user_pool_config, "default_action", null)
-      user_pool_id        = lookup(var.user_pool_config, "user_pool_id", null)
-      app_id_client_regex = lookup(var.user_pool_config, "app_id_client_regex", null)
-      aws_region          = lookup(var.user_pool_config, "aws_region", null)
+      default_action      = var.user_pool_config.default_action
+      user_pool_id        = var.user_pool_config.user_pool_id
+      app_id_client_regex = var.user_pool_config.app_id_client_regex
+      aws_region          = var.user_pool_config.aws_region
     }
   }
 
@@ -92,34 +103,58 @@ resource "aws_appsync_graphql_api" "api" {
       dynamic "openid_connect_config" {
         for_each = additional_authentication_provider.value == "OPENID_CONNECT" ? [1] : []
         content {
-          issuer    = lookup(var.openid_connect_config, "issuer", null)
-          auth_ttl  = lookup(var.openid_connect_config, "auth_ttl", null)
-          client_id = lookup(var.openid_connect_config, "client_id", null)
-          iat_ttl   = lookup(var.openid_connect_config, "iat_ttl", null)
+          issuer    = var.openid_connect_config.issuer
+          auth_ttl  = var.openid_connect_config.auth_ttl
+          client_id = var.openid_connect_config.client_id
+          iat_ttl   = var.openid_connect_config.iat_ttl
+        }
+      }
+      dynamic "lambda_authorizer_config" {
+        // Only add the 'lambda_authorizer_config' block if this is a Lambda mechanism
+        for_each = additional_authentication_provider.value == "AWS_LAMBDA" ? [1] : []
+        content {
+          authorizer_uri                   = var.lambda_authorizer_config.issauthorizer_uriuer
+          authorizer_result_ttl_in_seconds = var.lambda_authorizer_config.authorizer_result_ttl_in_seconds
+          identity_validation_expression   = var.lambda_authorizer_config.identity_validation_expression
         }
       }
       dynamic "user_pool_config" {
         // Only add the 'user_pool_config' block if this is a Cognito mechanism
         for_each = additional_authentication_provider.value == "AMAZON_COGNITO_USER_POOLS" ? [1] : []
         content {
-          user_pool_id        = lookup(var.user_pool_config, "user_pool_id", null)
-          app_id_client_regex = lookup(var.user_pool_config, "app_id_client_regex", null)
-          aws_region          = lookup(var.user_pool_config, "aws_region", null)
+          user_pool_id        = var.user_pool_config.user_pool_id
+          app_id_client_regex = var.user_pool_config.app_id_client_regex
+          aws_region          = var.user_pool_config.aws_region
         }
       }
     }
   }
 }
 
+// Create the custom domain, if desired
+resource "aws_appsync_domain_name" "this" {
+  count           = var.create_custom_domain == true ? 1 : 0
+  description     = var.custom_domain_description
+  domain_name     = var.custom_domain
+  certificate_arn = var.custom_domain_acm_certificate_arn
+}
+
+// Associate the custom domain, if one was created
+resource "aws_appsync_domain_name_api_association" "this" {
+  count       = var.create_custom_domain == true ? 1 : 0
+  api_id      = aws_appsync_graphql_api.this.id
+  domain_name = aws_appsync_domain_name.this[0].domain_name
+}
+
 // Get the log group that was automatically created by the GraphQL API
-resource "aws_cloudwatch_log_group" "api" {
-  name              = "/aws/appsync/apis/${aws_appsync_graphql_api.api.id}"
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/appsync/apis/${aws_appsync_graphql_api.this.id}"
   retention_in_days = var.log_retention_days
 }
 
 module "appsync_resolvers" {
   source             = "./modules/resolvers"
-  api_id             = aws_appsync_graphql_api.api.id
+  api_id             = aws_appsync_graphql_api.this.id
   datasources        = var.datasources
   unit_resolvers     = var.unit_resolvers
   functions          = var.functions
